572dae9ee3b892e84661b8eab4d3d7f31f866c48
fpeterfalvi <fpeterfalvi@gmail.com>
2018-05-11 03:56:49 -0700

    Introduction of mocking into the tests

--- src/test/java/hu/bme/mit/spaceship/GT4500Test.java
+++ src/test/java/hu/bme/mit/spaceship/GT4500Test.java
@@ -9,15 +9,23 @@ import org.junit.Test;
 public class GT4500Test {
 
   private GT4500 ship;
+  private TorpedoStore mockFirstTorpedoStore;
+  private TorpedoStore mockSecondTorpedoStore;
 
   @Before
   public void init(){
-    this.ship = new GT4500();
+    mockFirstTorpedoStore = mock(TorpedoStore.class);
+    mockSecondTorpedoStore = mock(TorpedoStore.class);
+    this.ship = new GT4500(mockFirstTorpedoStore, mockSecondTorpedoStore);
   }
 
   @Test
   public void fireTorpedo_Single_Success(){
     // Arrange
+    when(mockFirstTorpedoStore.isEmpty()).thenReturn(false);
+    when(mockFirstTorpedoStore.fire(1)).thenReturn(true);
+    when(mockSecondTorpedoStore.isEmpty()).thenReturn(false);
+    when(mockSecondTorpedoStore.fire(1)).thenReturn(true);
 
     // Act
     boolean result = ship.fireTorpedo(FiringMode.SINGLE);
@@ -29,6 +37,10 @@ public class GT4500Test {
   @Test
   public void fireTorpedo_All_Success(){
     // Arrange
+    when(mockFirstTorpedoStore.isEmpty()).thenReturn(false);
+    when(mockFirstTorpedoStore.fire(1)).thenReturn(true);
+    when(mockSecondTorpedoStore.isEmpty()).thenReturn(false);
+    when(mockSecondTorpedoStore.fire(1)).thenReturn(true);
 
     // Act
     boolean result = ship.fireTorpedo(FiringMode.ALL);
